7d01c705e186c31925c2e67bd06033bd9a2b123c
McNultyPT <mcnultypt@gmail.com>
2019-02-22 10:27:25 -0800

    mapping through projects in Projects component

--- projects/src/components/Projects.js
+++ projects/src/components/Projects.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Projects = props => {
     return (
         <div>
-
+           {props.projects.map(project => {
+               return (
+                   <Project
+                       name={project.name}
+                       description={project.description} 
+                   />
+               );
+           })} 
         </div>
     );
 }
