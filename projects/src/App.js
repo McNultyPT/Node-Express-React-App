21ad69d301e1a998fbbc21382b9f1cc73cd642af
McNultyPT <mcnultypt@gmail.com>
2019-02-22 10:16:54 -0800

    adding get methods to get projects and actions data

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [],
+      actions: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:4000/api/projects/')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          projects: res.data
+        })
+      })
+      .catch(err => console.log(err));
+    axios
+      .get('http://localhost:4000/api/actions')
+      .then(res => {
+        this.setState({
+          actions: res.data
+        })
+      });
+  }
+
   render() {
     return (
       <div className="App">
